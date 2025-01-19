1adca98c552d38e83e24c4f1285659031ffc1c02
raisul <raisul.islam@vivasoftltd.com>
2025-01-19 16:59:15 +0600

    priority queue update

--- heap/maxHeap.go
+++ heap/maxHeap.go
@@ -9,15 +9,15 @@ type MaxHeapPosition struct {
 
 type MaxHeap []MaxHeapPosition
 
-func (h MinHeap) Len() int           { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i].value >= h[j].value }
-func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i].value >= h[j].value }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *MinHeap) Push(x interface{}) {
+func (h *MaxHeap) Push(x interface{}) {
 	*h = append(*h, x.(MaxHeapPosition))
 }
 
-func (h *MinHeap) Pop() interface{} {
+func (h *MaxHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
